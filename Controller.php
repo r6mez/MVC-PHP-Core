c6ea844c986b361786e2fbb259c35b07294c76ee
Ramez Medhat <iramezdev@gmail.com>
2025-04-20 14:22:58 +0200

    initial commit

---
+++ Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Core;
+
+use App\Core\Middlewares\BaseMiddleware;
+
+class Controller {
+    public string $layout = 'main';
+    protected array $middlewares = [];
+    public string $action = '';
+
+    public function setLayout($layout){
+        $this->layout = $layout;
+    }
+
+
+    public function render($view, $params = []){
+        return Application::$app->view->renderView($view, $params);
+    }
+
+    public function registerMiddleware(BaseMiddleware $middleware){
+        $this->middlewares[] = $middleware;
+    }
+
+    public function getMiddlewares(){
+        return $this->middlewares;
+    }
+}
